04fe7692834b4c2ef0876a3dc9d64e6ac8894fb4
subin195-09 <k_subin@naver.com>
2022-05-18 18:27:36 +0900

    5/18 directory restructuring

---
+++ PPoong/back/src/user/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export default class User {
+  @PrimaryGeneratedColumn()
+    id: number;
+
+  @Column()
+    name: string;
+
+  @Column()
+    age: number;
+}
